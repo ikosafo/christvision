8042cea51966210881910c20bc06953da7e6c80d
Isaac <isaac@getinnotized.com>
2020-10-16 11:16:52 +0000

    Page title

--- ms/index.php
+++ ms/index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8"/>
 
-    <title>Admin | <?php echo $churcht ?></title>
+    <title>CVSI</title>
     <meta name="description" content="Latest updates and statistic charts">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
