927f578b63eb0ed8053418367a895866a9708926
Isaac <isaac@getinnotized.com>
2020-10-23 14:52:21 +0000

    db test

--- ms/admin/ajax/tables/member_table.php
+++ ms/admin/ajax/tables/member_table.php
@@ -62,7 +62,11 @@ $select_branch = $_POST['select_branch'];
             {data: 'residence'},
             {data: 'maritalstatus'},
             {data: 'id'}
-        ]
+        ],
+        responsive: !0,
+        dom: "<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>\n\t\t\t<'row'<'col-sm-12'tr>>\n\t\t\t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
+        buttons: ["print", "copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+        "bLengthChange": false,"order": []
     });
 
 
