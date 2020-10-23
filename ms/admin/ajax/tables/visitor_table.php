238da99fe2e8fe3a0bcf37fe9a0bd6a8b9f0cfb6
Isaac <isaac@getinnotized.com>
2020-10-23 15:02:21 +0000

    Admin membership

--- ms/admin/ajax/tables/visitor_table.php
+++ ms/admin/ajax/tables/visitor_table.php
@@ -59,16 +59,16 @@ $select_branch = $_POST['select_branch'];
             {data: 'residence'},
             {data: 'denomination'},
             {data: 'hearingabout'},
-            {data: 'description'},
+            {data: 'description'}
         ],
         responsive: !0,
         dom: "<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>\n\t\t\t<'row'<'col-sm-12'tr>>\n\t\t\t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
         buttons: ["print", "copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
-        "bLengthChange": false,"order": [],
-
-    }), $("#export_print").on("click", function (e) {
-        e.preventDefault(), t.button(0).trigger()
-    }), $("#export_copy").on("click", function (e) {
+        "bLengthChange": false,"order": []
+    }),
+        $("#export_print").on("click", function (e) {
+            e.preventDefault(), t.button(0).trigger()
+        }), $("#export_copy").on("click", function (e) {
         e.preventDefault(), t.button(1).trigger()
     }), $("#export_excel").on("click", function (e) {
         e.preventDefault(), t.button(2).trigger()
