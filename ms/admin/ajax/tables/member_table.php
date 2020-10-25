692dfcf02a571385583f2e573ec78112b2d28737
Isaac <isaac@getinnotized.com>
2020-10-25 07:04:47 +0000

    test

--- ms/admin/ajax/tables/member_table.php
+++ ms/admin/ajax/tables/member_table.php
@@ -47,6 +47,7 @@ $select_branch = $_POST['select_branch'];
     oTable =  $("#prov-table").DataTable({
         stateSave: true,
         "sDom": '<"top"ip>rt<"bottom"fl><"clear">',
+        "bLengthChange": false,"order": [],
         'processing': true,
         'serverSide': true,
         'serverMethod': 'post',
@@ -62,22 +63,7 @@ $select_branch = $_POST['select_branch'];
             {data: 'residence'},
             {data: 'maritalstatus'},
             {data: 'id'}
-        ],
-        responsive: !0,
-        dom: "<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>\n\t\t\t<'row'<'col-sm-12'tr>>\n\t\t\t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
-        buttons: ["print", "copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
-        "bLengthChange": false,"order": []
-    }),
-        $("#export_print").on("click", function (e) {
-        e.preventDefault(), t.button(0).trigger()
-    }), $("#export_copy").on("click", function (e) {
-        e.preventDefault(), t.button(1).trigger()
-    }), $("#export_excel").on("click", function (e) {
-        e.preventDefault(), t.button(2).trigger()
-    }), $("#export_csv").on("click", function (e) {
-        e.preventDefault(), t.button(3).trigger()
-    }), $("#export_pdf").on("click", function (e) {
-        e.preventDefault(), t.button(4).trigger()
+        ]
     });
 
 
