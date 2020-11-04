773a95c4ada4f8c54965532b7ec9a03fd2ddc59d
Isaac <isaac@getinnotized.com>
2020-11-04 13:39:48 +0000

    Admin SMS

--- ms/admin/ajax/tables/sms_table.php
+++ ms/admin/ajax/tables/sms_table.php
@@ -1,6 +1,5 @@
 <?php include('../../../../config.php');
-$branch = $_SESSION['branch'];
-$pinq = $mysqli->query("select * from sms where branch = '$branch' ORDER BY id DESC");
+$pinq = $mysqli->query("select * from sms ORDER BY id DESC");
 ?>
 <style>
     .dataTables_filter {
@@ -26,6 +25,7 @@ $pinq = $mysqli->query("select * from sms where branch = '$branch' ORDER BY id D
             <table id="data-table" class="table" style="margin-top: 3% !important;">
                 <thead>
                 <tr>
+                    <th>Branch</th>
                     <th>Group</th>
                     <th>Title</th>
                     <th>Message</th>
@@ -38,6 +38,20 @@ $pinq = $mysqli->query("select * from sms where branch = '$branch' ORDER BY id D
                 while ($fetch = $pinq->fetch_assoc()) {
                     ?>
                     <tr>
+                        <td>
+                            <?php
+                            $branchid = $fetch['branch'];
+                            $getbranch = $mysqli->query("select * from branch where id = '$branchid'");
+                            $resbranch = $getbranch->fetch_assoc();
+                            $branchname = $resbranch['name'];
+                            if ($branchid == "0") {
+                                echo "Admin";
+                            }
+                            else {
+                                echo $branchname;
+                            }
+                            ?>
+                        </td>
                         <td><?php echo $fetch['group']; ?></td>
                         <td><?php echo $fetch['title']; ?></td>
                         <td><?php echo $fetch['message']; ?></td>
@@ -58,89 +72,6 @@ $pinq = $mysqli->query("select * from sms where branch = '$branch' ORDER BY id D
 
     $('#data_search').keyup(function () {
         oTable.search($(this).val()).draw();
-    });
-
-    $(document).off('click', '.edit_service').on('click', '.edit_service', function () {
-        var theindex = $(this).attr('i_index');
-        //alert(theindex)
-        $.ajax({
-            type: "POST",
-            url: "ajax/forms/editform_service.php",
-            data: {
-                i_index: theindex
-            },
-            dataType: "html",
-            success: function (text) {
-                $('#servicesform_div').html(text);
-            },
-            complete: function () {
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                alert(xhr.status + " " + thrownError);
-            }
-        });
-    });
-
-    $(document).off('click', '.delete_service').on('click', '.delete_service', function () {
-        var theindex = $(this).attr('i_index');
-        //alert(theindex)
-        $.confirm({
-            title: 'Delete Service!',
-            content: 'Are you sure to continue?',
-            buttons: {
-                no: {
-                    text: 'No',
-                    keys: ['enter', 'shift'],
-                    backdrop: 'static',
-                    keyboard: false,
-                    action: function () {
-                        $.alert('Data is safe');
-                    }
-                },
-                yes: {
-                    text: 'Yes, Delete it!',
-                    btnClass: 'btn-blue',
-                    action: function () {
-                        $.ajax({
-                            type: "POST",
-                            url: "ajax/queries/delete_service.php",
-                            data: {
-                                i_index: theindex
-                            },
-                            dataType: "html",
-                            success: function (text) {
-                                $.ajax({
-                                    url: "ajax/tables/services_table.php",
-                                    beforeSend: function () {
-                                        KTApp.blockPage({
-                                            overlayColor: "#000000",
-                                            type: "v2",
-                                            state: "success",
-                                            message: "Please wait..."
-                                        })
-                                    },
-                                    success: function (text) {
-                                        $('#servicestable_div').html(text);
-                                    },
-                                    error: function (xhr, ajaxOptions, thrownError) {
-                                        alert(xhr.status + " " + thrownError);
-                                    },
-                                    complete: function () {
-                                        KTApp.unblockPage();
-                                    },
-                                });
-                            },
-
-                            complete: function () {
-                            },
-                            error: function (xhr, ajaxOptions, thrownError) {
-                                alert(xhr.status + " " + thrownError);
-                            }
-                        });
-                    }
-                }
-            }
-        });
     });
 
 </script>
